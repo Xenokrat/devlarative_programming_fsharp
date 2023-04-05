90be30ab7b9b491eef7e41c2def7e5d45a39abe8
Mantis <barabanovpv95@gmail.com>
2023-04-05 23:07:45 +0300

    lesson 9: fix

--- MainProject/Records.fs
+++ MainProject/Records.fs
@@ -2,12 +2,12 @@ type TimeOfDay = { hours: int; minutes: int; f: string }
 
 let (.>.) (x : TimeOfDay) (y : TimeOfDay) = 
     let xHours =
-    if x.f = "PM" then x.hours + 12
-    else x.hours
+        if x.f = "PM" then x.hours + 12
+        else x.hours
 
     let yHours =
-    if y.f = "PM" then y.hours + 12
-    else y.hours
-    { hours = xHours, minutes = x.minutes, f = x.f }
-    >
-    { hours = yHours, minutes = y.minutes, f = y.f }
+        if y.f = "PM" then y.hours + 12
+        else y.hours
+    let v1 = { hours = xHours; minutes = x.minutes; f = x.f }
+    let v2 = { hours = yHours; minutes = y.minutes; f = y.f }
+    v1 > v2
