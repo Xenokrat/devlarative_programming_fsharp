b9aa2b3b39ca8e62c4c69f0750ea0e6e855326e1
Mantis <barabanovpv95@gmail.com>
2023-04-18 08:55:40 +0300

    fix: set lesson

--- MainProject/Sets.fs
+++ MainProject/Sets.fs
@@ -3,7 +3,7 @@ let rec helperSubsets (n : int list) (k : int) =
     match (n, k) with
     | (_, 0) -> set [ Set.empty ]
     | ([], _) -> Set.empty
-    | (x :: xs, k) -> Set.union (Set.map (fun y -> Set.add x y) (allSubsets xs (k-1))) (allSubsets xs k)
+    | (x :: xs, k) -> Set.union (Set.map (fun y -> Set.add x y) (helperSubsets xs (k-1))) (helperSubsets xs k)
 
 let rec allSubsets (n : int) (k : int) =
     helperSubsets [ 1..n ] k
