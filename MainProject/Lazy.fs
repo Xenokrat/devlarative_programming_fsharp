e90cea282ed518da67169c109bbddf459b5eb692
Mantis <barabanovpv95@gmail.com>
2023-05-02 10:53:25 +0300

    fix: remain only nessesary code

--- MainProject/Lazy.fs
+++ MainProject/Lazy.fs
@@ -1,24 +1,17 @@
 type 'a cell = Nil | Cons of 'a * Lazy<'a cell>
 
-let rec nat (n : int) : int cell = 
-    Cons (n, lazy(nat(n+1)))
-
 let hd (s : 'a cell) : 'a =
   match s with
-  | Nil -> failwith "hd"
+    Nil -> failwith "hd"
   | Cons (x, _) -> x
 
 let tl (s : 'a cell) : Lazy<'a cell> =
   match s with
-  | Nil -> failwith "tl"
+    Nil -> failwith "tl"
   | Cons (_, g) -> g
-
 
 // 51.3
 let rec nth (s : 'a cell) (n : int) : 'a =
     match s, n with
     | s, 0 -> hd s
     | _ -> nth ((tl s).Force()) (n - 1)
-
-// например, получить 30000-й элемент:
-// nth n0 30000
